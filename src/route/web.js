4f52fe1ef372e172b680f886cd275a7739e50d54
tsthuy <huydo282003@gmail.com>
2023-10-21 17:45:05 +0700

    Write API with Post-man

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
@@ -11,6 +12,10 @@ let initWebRoutes = (app) => {
     router.get('/edit-crud', homeController.getEditCRUD);
     router.post('/put-crud', homeController.putCRUD);
     router.get('/delete-crud', homeController.deleteCRUD);
+
+
+
+    router.post('/api/login', userController.handleLogin);
     return app.use('/', router);
 }
 module.exports = initWebRoutes;
---
+++ src/services/userService.js
@@ -0,0 +1,73 @@
+import db from "../../models";
+import bcrypt, { hash } from 'bcryptjs';
+
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {}
+            let isExist = await checkUserEmail(email);
+            if (isExist) {
+                // user already exist
+                //compare
+
+                let user = await db.User.findOne({
+                    attributes: ['email', 'roleId', 'password'],
+                    where: {
+                        email: email,
+
+                    },
+                    raw: true
+                })
+                if (user) {
+                    //compare
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.errMessage = 'check pass done';
+                        console.log(user)
+                        delete user.password;
+                        userData.user = user;
+                    }
+                    else {
+                        userData.errCode = 3;
+                        userData.errMessage = 'Wrong pass';
+                    }
+                }
+                else {
+                    userData.errCode = 2;
+                    userData.errMessage = `User's is not found`
+                }
+            }
+            else {
+                userData.errCode = 1;
+                userData.errMessage = `Yours email isn't exist in mt system. Phy try other email!`
+            }
+            resolve(userData)
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let checkUserEmail = (UserEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: {
+                    email: UserEmail,
+                }
+            })
+            if (user) {
+                resolve(true)
+            }
+            else {
+                resolve(false)
+            }
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+module.exports = {
+    handleUserLogin: handleUserLogin,
+}
